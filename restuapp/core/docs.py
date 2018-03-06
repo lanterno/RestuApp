ee0f29a214573eb94546854a5a5878f293e758be
Ahmed S. Elghareeb <ahmeds.elghareeb@gmail.com>
2018-03-06 22:29:19 +0200

    Add core app with documentation view

---
+++ restuapp/core/docs.py
@@ -0,0 +1,13 @@
+from coreapi import Document
+
+
+BASE_URL = '/api/v1/'
+
+SCHEMA = Document(
+    title='restuapp.someplace.com API',
+    description="""
+        RestuApp, a resturant app that's app about testing REST APIs.
+    """,
+    content={
+    }
+)
